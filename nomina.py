b9cfcb36ff902f531aec71dcefca898eb8f6599f
ingridca8 <149647031+ingridca8@users.noreply.github.com>
2024-02-23 12:28:00 -0600

    Update nomina.py

--- nomina.py
+++ nomina.py
@@ -25,7 +25,7 @@ with open('reporte_csv.csv', mode='r') as csv_file:
     checks_cleaned.append(checks[6])
     checks_cleaned.append(checks[8])
     checks_cleaned.append(checks[10])
-    checks_cleaned.append(checks[14])
+    checks_cleaned.append(checks[12])
     checks_splitted = []
     lista_separada = []
     for i in checks_cleaned:
@@ -76,9 +76,9 @@ with open('reporte_csv.csv', mode='r') as csv_file:
     schedule['Alonso'] = lista_separada[5]
     schedule['Ingrid'] = lista_separada[6]
 
-    print('Total de horas trabajadas:\n', schedule)
+    print('Total de minutos trabajadas:\n', schedule)
 
-    
+
 
     for key in schedule:
         lista = schedule[key]
@@ -89,21 +89,27 @@ with open('reporte_csv.csv', mode='r') as csv_file:
                 days_dict[i]=lista[ind+1]
         schedule[key] = days_dict
     
-    def missing_hours(nombre):
-        entrada = input('\n\nIntroduzca hora de entrada para {nombre} el día {dia} (formato 24hrs 00:00): '.format(nombre=nombre, dia=key))
-        salida = input('Introduzca hora de salida para {nombre} el día {dia} (formato 24hrs 00:00): '.format(nombre=nombre, dia=key))
-        entrada_hrs = int(entrada[:2])
-        entrada_min = int(entrada[3:])
-        entrada_conv = entrada_hrs*60+entrada_min
-        salida_hrs = int(salida[:2])
-        salida_min = int(salida[3:])
-        salida_conv = salida_hrs*60+salida_min
-        min_trabajados = salida_conv - entrada_conv
+    def missing_hours(nombre, dia):
+        min_trabajados = 0
+        entrada = input('\n\nIntroduzca hora de entrada para {nombre} el día {dia} (formato 24hrs 00:00): '.format(nombre=nombre, dia=dia))
+        if entrada == '0':
+            min_trabajados = 1
+        elif entrada == '1':
+            min_trabajados = 600
+        else:
+            salida = input('Introduzca hora de salida para {nombre} el día {dia} (formato 24hrs 00:00): '.format(nombre=nombre, dia=dia))
+            entrada_hrs = int(entrada[:2])
+            entrada_min = int(entrada[3:])
+            entrada_conv = entrada_hrs*60+entrada_min
+            salida_hrs = int(salida[:2])
+            salida_min = int(salida[3:])
+            salida_conv = salida_hrs*60+salida_min
+            min_trabajados = salida_conv - entrada_conv
         if 0 < min_trabajados:
             return min_trabajados
         else:
             print('Introduzca una entada valida')
-            missing_hours(nombre)
+            missing_hours(nombre, dia)
 
     def sueldo_semanal(nombre):
         sueldo_semanal= 0
@@ -113,7 +119,7 @@ with open('reporte_csv.csv', mode='r') as csv_file:
             if 0 < min_trabajados:
                 min_trabajados_semana += min_trabajados
             else:
-                min_trabajados_semana += missing_hours(nombre)    
+                min_trabajados_semana += missing_hours(nombre, key)    
             if min_trabajados_semana <= 3000:
                 sueldo_semanal = min_trabajados_semana*salarios[nombre]
             else:
@@ -124,6 +130,45 @@ with open('reporte_csv.csv', mode='r') as csv_file:
         print('Minutos trabajados por día:\n',schedule[nombre])
         print('Sueldo semanal:',sueldo_semanal)
 
+
+    def salario_ingrid():
+        sueldo_semanal_ingrid = 0
+        sueldo_diario = 0
+        for key in schedule['Ingrid']: #360 diarios
+            min_trabajados = schedule['Ingrid'][key] 
+            if min_trabajados <= 540 and 0 < min_trabajados:
+                sueldo_diario = min_trabajados*salarios['Ingrid']
+            elif min_trabajados == 0:
+                entrada = input('\n\nIntroduzca hora de entrada para {nombre} el día {día} (formato 24hrs 00:00): '.format(nombre='Ingrid', día=key))
+                if entrada == '0':
+                    min_trabajados = 1
+                elif entrada == '1':
+                    min_trabajados = 540
+                else:
+                    salida = input('Introduzca hora de salida para {nombre} el día {día} (formato 24hrs 00:00): '.format(nombre='Ingrid', día=key))
+                    entrada_hrs = int(entrada[:2])
+                    entrada_min = int(entrada[3:])
+                    entrada_conv = entrada_hrs*60+entrada_min
+                    salida_hrs = int(salida[:2])
+                    salida_min = int(salida[3:])
+                    salida_conv = salida_hrs*60+salida_min
+                    min_trabajados = salida_conv - entrada_conv
+                if min_trabajados <= 540 and 0 < min_trabajados:
+                    sueldo_diario = min_trabajados*salarios['Ingrid']
+                else:
+                    tiempo_extra = min_trabajados-540
+                    sueldo_tiempo_extra = tiempo_extra*salarios['Ingrid']*2
+                    sueldo_diario = salarios['Ingrid']*540 + sueldo_tiempo_extra
+            else:
+                tiempo_extra = min_trabajados-540
+                sueldo_tiempo_extra = tiempo_extra*salarios['Ingrid']*2
+                sueldo_diario = salarios['Ingrid']*540 + sueldo_tiempo_extra
+            sueldo_semanal_ingrid += sueldo_diario
+            schedule['Ingrid'][key] = sueldo_diario
+        print('\n Ingrid')
+        print('Salario por día:\n',schedule['Ingrid'])
+        print('Sueldo semanal:',sueldo_semanal_ingrid)
+
     #Salario por minuto normal
     salarios = {
         'Yesly': 2200/3000, 
@@ -131,47 +176,28 @@ with open('reporte_csv.csv', mode='r') as csv_file:
         'Sandry': 1800/3000,
         'Angel': 3500/3000, #seguro 3694.39 a la quincena
         'Alonso': 2300/3000,
-        'Ingrid': 1800/3000, 
+        'Ingrid': 1800/2700, 
     }
     #Tiempo extra es el doble 
 
-    for key in salarios:
-        sueldo_semanal(key)
-    
+    def todos_los_salarios():
+        for key in salarios:
+            sueldo_semanal(key)
+        salario_ingrid()
 
-    #SALARIO INGRID
-    sueldo_semanal_ingrid = 0
-    for key in schedule['Ingrid']: #360 diarios
-        min_trabajados = schedule['Ingrid'][key] 
-        if min_trabajados <= 540 and 0 < min_trabajados:
-            sueldo_diario = min_trabajados*salarios['Ingrid']
-        elif min_trabajados == 0:
-            entrada = input('\n\nIntroduzca hora de entrada para {nombre} el día {día} (formato 24hrs 00:00): '.format(nombre='Ingrid', día=key))
-            salida = input('Introduzca hora de salida para {nombre} el día {día} (formato 24hrs 00:00): '.format(nombre='Ingrid', día=key))
-            entrada_hrs = int(entrada[:2])
-            entrada_min = int(entrada[3:])
-            entrada_conv = entrada_hrs*60+entrada_min
-            salida_hrs = int(salida[:2])
-            salida_min = int(salida[3:])
-            salida_conv = salida_hrs*60+salida_min
-            min_trabajados = salida_conv - entrada_conv
-            if min_trabajados <= 540 and 0 < min_trabajados:
-                sueldo_diario = min_trabajados*salarios['Ingrid']
-            else:
-                tiempo_extra = min_trabajados-540
-                sueldo_tiempo_extra = tiempo_extra*salarios['Ingrid']*2
-                sueldo_diario = salarios['Ingrid']*540 + sueldo_tiempo_extra
+    
+    def salario_especifico():
+        consulta = input("\n¿De quien quieres consultar el sueldo? \n")
+        if consulta == 'todos':
+           todos_los_salarios()
+        elif consulta == 'Ingrid':
+            salario_ingrid()
         else:
-            tiempo_extra = min_trabajados-540
-            sueldo_tiempo_extra = tiempo_extra*salarios['Ingrid']*2
-            sueldo_diario = salarios['Ingrid']*540 + sueldo_tiempo_extra
-        sueldo_semanal_ingrid += sueldo_diario
-        schedule['Ingrid'][key] = sueldo_diario
-    print('\n Ingrid')
-    print('Salario por día:\n',schedule['Ingrid'])
-    print('Sueldo semanal:',sueldo_semanal_ingrid)
+            if consulta in salarios:
+                sueldo_semanal(consulta)
+            else: 
+                print('Introduzca una entrada válida')
+                salario_especifico()
 
 
-
-
-    
+    salario_especifico()
